4d392260eea9e82eb6a32439d7a9b0cb08527e56
lukablaskovic <lukablaskovic2000@gmail.com>
2022-10-11 23:42:54 +0200

    Riješen Zadatak2

--- zadaci/01-zadaci/Zadatak1.py
+++ zadaci/01-zadaci/Zadatak1.py
@@ -1,8 +1,11 @@
 """
 Zadatak 1
+
 Kreiraj funkciju koja uzima listu brojeva i vraca novu listu brojeva koji
 su višekratnici broja 4, ako nisu ih korjenuje i zaokruzi na 2 decimale.
 (One-liner u return-u)
+
+Ispis: [213,14,12,6543,232] -> [14.59, 3.74, 12, 80.89, 232]
 """
 list = [213,14,12,6543,232]
 def fun(x):
