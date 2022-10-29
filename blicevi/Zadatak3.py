3fada5213fb4dc78f7050cb789663772e6592bb6
Luka Blašković <59925899+lukablaskovic@users.noreply.github.com>
2022-10-29 14:38:45 +0200

    Delete Zadatak3.py

--- blicevi/Zadatak3.py
+++
@@ -1,6 +0,0 @@
-lis = [
-    {"naziv" : "Burek", "kategorija": "pite", "ocjena" : 1},
-    {"naziv" ; "Ramsteak", "kategorija" : "steak", "ocjena" : 9},
-    {"naziv" : "Ribeye", "kategorija" : "steak", "ocjena" : 4},
-    {"naziv" : "Sirnica", "kateogrija" : "pite", "ocjena" : 5}
-    ]
