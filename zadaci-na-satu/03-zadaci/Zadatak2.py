3f4f152086b6122024fc8cc75ad389ddf674c7ec
Luka <lukablaskovic2000@gmail.com>
2022-10-29 14:54:19 +0200

    Dodan tekst zadatka

--- zadaci-na-satu/03-zadaci/Zadatak2.py
+++ zadaci-na-satu/03-zadaci/Zadatak2.py
@@ -1,3 +1,20 @@
+"""
+2. Kreirajte tri asinkrone funkcije te funkciju main. Prva funkcija uzima
+listu korisnika i provjeri radi li se o listi. Ovisno o veličini liste, svakom
+korisniku dodijeli id po redu. Potom vraća listu dictonary-a korisnika.
+[{“korisnik”:“Ivan”,“id”:0},{“korisnik”:“Pero”,“id”:1}]
+
+Druga funkcija ispisuje brojeve od 1 do 10. Ceka 0.01 sekundu poslje
+svakog broja. Treća funkcija uzima listu dictonary-a korisnika i provjeri
+radi li se o listi i jesu li svi elemnti dictionary. Također provjerava postoje
+li ključevi korisnik i id. Čeka 0.05 sekundi. Kreira i nakraju vraća listu
+tuple-a korisnika gdje dodatno svakom korisniku izraćuna duljinu imena.
+[(“Ivan”,0,4), (“Pero”,1,4)]
+
+Funkcija main prvo kreira listu imena korisnika i poziva redom funkcije, te
+zavrsetkom treće funkcije zavrsava i program.
+"""
+
 import asyncio
 
 async def func1(k):
