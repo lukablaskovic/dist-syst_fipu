30754e14e576f6540ec6b9317eb2a74d032e3615
Luka <lukablaskovic2000@gmail.com>
2022-11-18 11:55:43 +0100

    Rad na servisu3

--- zadace/04-zadace/Servis2.py
+++ zadace/04-zadace/Servis2.py
@@ -26,9 +26,12 @@ async def parseActivities(req):
         json_data = await req.json()
         received_activities.append(json_data)
         
+        #Filter and group activities by type
         charity = [activity for activity in received_activities if activity["type"] == "charity"]
         recreational = [activity for activity in received_activities if activity["type"] == "recreational"]
         other = [activity for activity in received_activities if activity["type"] != "recreational" and activity["type"] != "charity"]
+        
+        received_activities = []
 
         print("charity:", charity)
 
@@ -36,9 +39,15 @@ async def parseActivities(req):
 
         print("other:", other)
 
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+            for i in range(len(other)):
+                asyncio.create_task(session.post("http://0.0.0.0:8085/", json = other[i]))
+
         return web.json_response({"status" : "OK"}, status=200)        
     except Exception as e:
         return web.json_response({"failed" : str(e)}, status=500)         
+
+
 
 app = web.Application()
 
--- zadace/04-zadace/Servis3.py
+++ zadace/04-zadace/Servis3.py
@@ -1,7 +1,48 @@
 """
 Servis 3
+Charity i Recreational se šalju na posebnu
+rutu trećeg servisa (/charityAndRecreational), ostale se šalju na običnu
+rutu (/otherActivities).
 Treći servis sprema svaku aktivnost i tip u
 temporary storage. Ukoliko se radi o ruti /otherActivities, za svaku
 aktivnost šalje zahtjev na https://randomuser.me/api/ od kojeg uzme
 ime, prezime i datum rođenja te pridoda ih kao nove ključeve aktivnosti.
 """
+
+import aiohttp
+import asyncio
+import time
+
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+@routes.post("/charityAndRecreational")
+async def parseCharRec(req):
+    pass
+
+@routes.post("/otherActivities")
+async def parseOther(req):
+    json_data = await req.json()
+    print(json_data)
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+            task = asyncio.create_task(sendRequest(json_data, session))
+            users = await task
+            print(users)
+    pass
+
+#Send request to randomuser.me api
+async def sendRequest(activities, session):
+    random_users = []
+    for a in range(len(activities)):
+        random_users.append(asyncio.create_task(session.get("https://randomuser.me/api/")))
+        print("ran - req sent! - ", a)
+    res = await asyncio.gather(*random_users)
+    res = [await x.json() for x in res]
+    return res
+
+app = web.Application()
+
+app.router.add_routes(routes)
+
+web.run_app(app, port=8085)
