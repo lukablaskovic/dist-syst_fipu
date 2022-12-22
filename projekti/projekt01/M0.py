0972a331ce1b28a4deafdcbe59f7568afae4390f
Luka <lukablaskovic2000@gmail.com>
2022-12-22 19:03:52 +0100

    M0 prakticki gotov

--- projekti/projekt01/M0.py
+++ projekti/projekt01/M0.py
@@ -3,7 +3,7 @@ import aiosqlite
 import asyncio
 
 import pandas as pd
-
+import numpy as np
 
 from aiohttp import web
 
@@ -14,14 +14,42 @@ routes = web.RouteTableDef()
 async def getGithubLinks(req):
     try:
         async with aiosqlite.connect("projekti/projekt01/data.db") as db:
-            # Check if table is empty
+            # SQL Query which checks if table is empty
+            # Return: 0 - empty, 1 - not empty
             async with db.execute("SELECT COUNT(1) WHERE EXISTS (SELECT * FROM data)") as cur:
                 async for row in cur:
-                    print(row)
-                    await fillDB()
-        return web.json_response({"M0": "OK"}, status=200)
+                    if tableIsEmpty(row[0]):
+                        await fillDB()
+                    else:
+                        data = await fetchRandomRows(db)
+                        print(data)
+
+        return web.json_response({"M0": "OK", "payload": data}, status=200)
     except Exception as e:
         return web.json_response({"M0": str(e)}, status=500)
+
+
+def tableIsEmpty(countResult):
+    return True if countResult == 0 else False
+
+
+async def fetchRandomRows(conn):
+    cursor = await conn.cursor()
+    # Get the total number of rows in the table
+    await cursor.execute("SELECT COUNT(*) FROM data")
+    (total_rows,) = await cursor.fetchone()
+
+    # Generate a list of 100 random row indices
+    row_indices = np.random.randint(0, total_rows, size=100)
+
+    print(row_indices)
+
+    # Fetch the rows with the randomly selected indices
+    rows = []
+    for row_index in row_indices:
+        await cursor.execute("SELECT * FROM data LIMIT 1 OFFSET {row_index}".format(row_index=row_index))
+        rows.append(await cursor.fetchone())
+    return rows
 
 
 async def fillDB():
